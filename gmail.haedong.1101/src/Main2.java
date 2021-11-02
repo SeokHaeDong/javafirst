940a27ce02986476968de2e1a6a2a19b782cbd64
haedong <haedong@192.168.20.127>
2021-11-02 19:46:27 +0900

    Java study .2

--- gmail.haedong.1101/src/Main2.java
+++ gmail.haedong.1101/src/Main2.java
@@ -9,7 +9,13 @@ public class Main2 {
 		food_brand chicken_brand = new food_brand();
 		chicken_brand.brande_name = "BHC";
 		chicken_brand.best_menu = "후라이드";
-		chicken_brand.sub_menu = new String[]{"감튀","치즈볼","윙"};
+		chicken_brand.side_menu = new String[]{"감튀","치즈볼","윙"};
+		
+//		for(int i = 0; i < 3; i++) {
+//			chicken_brand.side_menu[i] = new String();
+//		}
+		
+		chicken_brand.side_menu[0] = "양념감튀";
 		chicken_brand.price = 16000;
 		
 		food_brand chicken_brand1 = new food_brand();
@@ -20,7 +26,7 @@ public class Main2 {
 		
 		System.out.println(food_brand.type);
 
-		System.out.println(chicken_brand.brande_name + "=" + chicken_brand.best_menu + " : " + chicken_brand.price + "원 / " + chicken_brand.type + " / " +chicken_brand.sub_menu[0] );
+		System.out.println(chicken_brand.brande_name + "=" + chicken_brand.best_menu + " : " + chicken_brand.price + "원 / " + chicken_brand.type + " / " +chicken_brand.side_menu[0] );
 		System.out.println(chicken_brand1.brande_name + "=" +chicken_brand1.best_menu + " : " + chicken_brand1.price + "원 / " + chicken_brand1.type );
 	}
 
--- gmail.haedong.1101/src/food_brand.java
+++ gmail.haedong.1101/src/food_brand.java
@@ -4,8 +4,8 @@ public class food_brand {
 	public String brande_name;
 	
 	public String best_menu;
-	
-	public String [] sub_menu;
+	//main 배열 인스턴스를 생성하듯이 반복문을 통하여 대입을 하여 주어야 한다 OR Java 정규식을 통하여 new String[] {}을 통하여 초기화 밑 선언을 동시에 진행한다 
+	public String [] side_menu;
 	
 	public int price;
 	
